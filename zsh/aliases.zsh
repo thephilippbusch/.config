5364aca0cfdc73eee914f44211c2825a6a75e4a2
Philipp Busch <philippleonbusch@gmail.com>
2025-10-02 01:14:05 +0200

    add zsh configuration

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+alias k="kubectl"
+alias dc="docker-compose"
+alias mk="minikube"
+
+alias code="open -a 'Visual Studio Code'"
+
+# alias ls="exa --icons --group-directories-first"
+# alias ll="exa --icons --group-directories-first -l"

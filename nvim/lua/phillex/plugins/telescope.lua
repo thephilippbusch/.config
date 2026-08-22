57072ce86fbdc72a43b489c8923bd9d2c6ad16eb
thephilippbusch <phbu@netlight.com>
2026-08-22 16:52:33 +0200

    add telescope shortcuts to nvim config

--- nvim/lua/phillex/plugins/telescope.lua
+++ nvim/lua/phillex/plugins/telescope.lua
@@ -38,6 +38,8 @@ return {
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>fhc", "<cmd>Telescope command_history<cr>", { desc = "Find last used commands" })
+		keymap.set("n", "<leader>fhs", "<cmd>Telescope search_history<cr>", { desc = "Find last used searches" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 		keymap.set(
 			"n",
